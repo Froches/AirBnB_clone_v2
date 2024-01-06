c7d36d085260ad4f3558da872becd340838b4431
Froches <michaelfavour418@gmail.com>
2024-01-06 15:25:27 -0800

    setup web static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,25 +1,30 @@
 #!/usr/bin/env bash
-#Sets up your web servers for the deployment of web_static.
-sudo apt get update
-sudo apt install nginx
-mkdir /data/
-mkdir /data/web_static
-mkdir /data/web_static/releases
-mkdir /data/web_static/shared
-mkdir /data/web_static/releases/test
-touch /data/web_static/releases/test/index.html
-echo "This is some content to test my nginx configuration"> /data/web_static/releases/test/index.html
-rm -f /data/web_static/current && ln -s /data/web_static/releases/test/ /data/web_static/current
+
+# Install Nginx if not installed
+if ! command -v nginx &> /dev/null; then
+    sudo apt-get update
+    sudo apt-get install -y nginx
+fi
+
+# Create necessary folders if they don't exist
+sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
+sudo touch /data/web_static/releases/test/index.html
+
+# Create a simple HTML content for the test index.html file
+echo "<html><head><title>Test Page</title></head><body>Test Page</body></html>" | sudo tee /data/web_static/releases/test/index.html
+
+# Remove existing symbolic link and create a new one
+sudo rm -rf /data/web_static/current
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+# Set ownership to the ubuntu user and group
 sudo chown -R ubuntu:ubuntu /data/
-echo "
-server {
-    # Other configurations...
 
-    location /hbnb_static {
-        alias /data/web_static/current/;
-    }
+# Update Nginx configuration to serve content to hbnb_static
+config_text="location /hbnb_static/ {\n\talias /data/web_static/current/;\n}\n"
+sudo sed -i "/server_name _;/a $config_text" /etc/nginx/sites-available/default
 
-    # Other configurations...
-}
-" > /etc/nginx/sites-available/
-sudo service restart nginx
+# Restart Nginx
+sudo service nginx restart
+
+exit 0
