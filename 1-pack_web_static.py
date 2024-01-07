857151fe3b170ab1ee5a992460580c972e809a24
Froches <michaelfavour418@gmail.com>
2024-01-06 16:15:04 -0800

    compress

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -8,22 +8,21 @@ import os
 
 
 def do_pack():
-    # Define the current date and time for the archive name
+    """
+    Creates new archive from contents of the web static folder
+    """
+
     now = datetime.utcnow()
     timestamp = now.strftime("%Y%m%d%H%M%S")
 
-    # Create the versions folder if it doesn't exist
     if not os.path.exists("versions"):
         os.makedirs("versions")
 
-    # Name of the archive
     archive_name = f"web_static_{timestamp}.tgz"
     archive_path = f"versions/{archive_name}"
 
-    # Create the .tgz archive
     result = local(f"tar -cvzf {archive_path} web_static")
 
-    # Check if the archive has been correctly generated
     if result.failed:
         return None
     else:
